16dc9b89c566653b2541b3c1f689252984a1a34d
Nilson Nascimento <nnascimento@redzonemap.com>
2018-04-10 21:03:55 -0400

    Add Deep Linking

--- iDevReader/AppDelegate.swift
+++ iDevReader/AppDelegate.swift
@@ -15,15 +15,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var appCoordinator: AppCoordinator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+
         window = UIWindow()
         appCoordinator = AppCoordinator(window: window!)
         appCoordinator.start()
+
+//        appCoordinator.route(to: .bookmarks)
+//        appCoordinator.route(to: .article(article: BookmarkStore().items[0]))
+//        appCoordinator.route(to: .bookmarks)
+
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         
-        appCoordinator.route(to: .bookmarks)
-        appCoordinator.route(to: .article(article: BookmarkStore().items[0]))
-        appCoordinator.route(to: .bookmarks)
+        guard url.pathComponents == ["/", "articles"],
+            let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems,
+            let urlString = queryItems.first(where: { $0.name == "url" })?.value,
+            let articleUrl = URL(string: urlString) else { return false }
+        
+        appCoordinator.route(to: .article(article: BookmarkStore().items[1]))
         
         return true
     }
+
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        appCoordinator.route(to: .bookmarks)
+    }
+
 }
